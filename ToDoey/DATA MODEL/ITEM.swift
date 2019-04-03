b4f24f11cdb755be34a35597476f827d060dac5b
丁伟华 <livewithliver@hotmail.com>
2019-04-03 14:59:46 +1100

    做到 restruct 这一步

---
+++ ToDoey/DATA MODEL/ITEM.swift
@@ -0,0 +1,14 @@
+//
+//  ITEM.swift
+//  ToDoey
+//
+//  Created by User on 2019/4/3.
+//  Copyright © 2019 Frank Ding. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title : String = ""
+    var done : Bool = true
+    
+}
