88efae667c5c2522de21e442a7131fa754dcfe9a
丁伟华 <livewithliver@hotmail.com>
2019-04-07 19:43:58 +1000

    257 making our cells swipable

--- ToDoey/controllers/CategoryTableViewController.swift
+++ ToDoey/controllers/CategoryTableViewController.swift
@@ -9,14 +9,16 @@
 import UIKit
 import CoreData
 import RealmSwift
+import SwipeCellKit
 
 class CategoryTableViewController: UITableViewController {
     var categories : Results<Category>!
     let realm = try! Realm()
     
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 150.0
         loadCategories()
     }
     //MARK - TABLEVIEW DELEGATE Method
@@ -35,9 +37,16 @@ class CategoryTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories?.count ?? 1
     }
+    //    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SwipeTableViewCell
+    //        cell.delegate = self
+    //        return cell
+    //    }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! SwipeTableViewCell
+        cell.delegate = self
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Item Added"
         return cell
@@ -55,12 +64,12 @@ class CategoryTableViewController: UITableViewController {
     }
     func loadCategories() {
         categories = realm.objects(Category.self)
-//        let request : NSFetchRequest<Category> = Category.fetchRequest()
-//        do {
-//            categories = try context.fetch(request)
-//        }catch{
-//            print("\(error)")
-//        }
+        //        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        //        do {
+        //            categories = try context.fetch(request)
+        //        }catch{
+        //            print("\(error)")
+        //        }
         tableView.reloadData()
     }
     
@@ -83,9 +92,38 @@ class CategoryTableViewController: UITableViewController {
         })
         present(alert, animated: true, completion: nil)
     }
-    
-    
-    
+}
+
+// MARK - SWIPE CELL DELEGATE METHODS
+extension CategoryTableViewController: SwipeTableViewCellDelegate {
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") {action, indexPath in
+            // handle action by updating model with deletion
+            
+            if let categoryForDeletion = self.categories?[indexPath.row] {
+                do{
+                    try self.realm.write {
+                        self.realm.delete(categoryForDeletion)
+                    }
+                }catch{
+                    print("\(error)")
+                }
+            }
+        }
+        
+//        tableView.reloadData()
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "trash-circle")
+        
+        return [deleteAction]
+    }
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
     
 }
 
